c58da45ec26d926662d07ced3c707889a6d23362
Andrei <drang.andray@gmail.com>
2017-10-30 04:51:34 +0300

    Добавлен асинхронный модуль для решения капчи изображением + пример к нему

--- aio_test.py
+++ aio_test.py
@@ -1,6 +1,10 @@
-from python3_anticaptcha import aioAntiCaptchaControl
-
+from python3_anticaptcha import aioAntiCaptchaControl, aioImageToTextTask
+import asyncio
+import aiohttp
 import time
 print(aioAntiCaptchaControl.AntiCaptchaControl(anticaptcha_key='ae23fffcfaa29b170e3843e3a486ef19').get_balance())
 
-print(aioAntiCaptchaControl.AntiCaptchaControl(anticaptcha_key='ae23fffcfaa29b170e3843e3a486ef19').complaint_on_result(reported_id=-7))
+#print(aioAntiCaptchaControl.AntiCaptchaControl(anticaptcha_key='ae23fffcfaa29b170e3843e3a486ef19').complaint_on_result(reported_id=-7))
+
+print(aioImageToTextTask.ImageToTextTask(anticaptcha_key='ae23fffcfaa29b170e3843e3a486ef19', save_format = 'const')\
+									.captcha_handler('http://85.255.8.26/static/image/common_image_example/800070.png'))
