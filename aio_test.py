366086b7107696e50656a64717a848da9af74b8b
Andrei <drang.andray@gmail.com>
2017-10-30 15:00:14 +0300

    Болванка для тестов

--- aio_test.py
+++ aio_test.py
@@ -1,10 +1,18 @@
-from python3_anticaptcha import aioAntiCaptchaControl, aioImageToTextTask
+from python3_anticaptcha import NoCaptchaTaskProxyless
 import asyncio
 import aiohttp
 import time
-print(aioAntiCaptchaControl.AntiCaptchaControl(anticaptcha_key='ae23fffcfaa29b170e3843e3a486ef19').get_balance())
 
-#print(aioAntiCaptchaControl.AntiCaptchaControl(anticaptcha_key='ae23fffcfaa29b170e3843e3a486ef19').complaint_on_result(reported_id=-7))
 
-print(aioImageToTextTask.ImageToTextTask(anticaptcha_key='ae23fffcfaa29b170e3843e3a486ef19', save_format = 'const')\
-									.captcha_handler('http://85.255.8.26/static/image/common_image_example/800070.png'))
+async def run():
+	try:
+		result = await /функция асинхронная/
+		print(result)
+	except Exception as err:
+		print(err)
+
+
+if __name__ == '__main__':
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(run())
+	loop.close()
