39269c3214b003feb1c43469acf79d95e633fc04
Andrei <drang.andray@gmail.com>
2018-11-01 18:19:27 +0300

    Добавление в конфиг параметров для подключения к стандартному callback серверу

--- python3_anticaptcha/config.py
+++ python3_anticaptcha/config.py
@@ -10,6 +10,21 @@ incorrect_captcha_url = "https://api.anti-captcha.com/reportIncorrectImageCaptch
 get_queue_status_url = "https://api.anti-captcha.com/getQueueStats"
 # ключ приложения
 app_key = "867"
+
+"""
+Параметры для callback
+"""
+# IP для работы callback`a
+HOST = '85.255.8.26'
+# PORT для работы callback`a
+PORT = 8001
+#данные для подключения к RabbitMQ на callback сервере
+RTMQ_USERNAME = 'hardworker_1'
+RTMQ_PASSWORD = 'password'
+RTMQ_HOST = '85.255.8.26'
+RTMQ_PORT = 5672
+RTMQ_VHOST = 'anticaptcha_vhost'
+
 # random user agent data
 # получаем рандомный userAgent
 # TODO протестировать `fake_useragent` на исправление старых беспричинных падений и обновить получение данных
