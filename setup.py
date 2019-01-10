f04d3874a5a3c69198a7c1325cb491c9889476bf
Andrei <drang.andray@gmail.com>
2019-01-11 01:06:30 +0300

    New version release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name = 'python3-anticaptcha',
-    version = '1.1',
+    version = '1.2',
     author = 'AndreiDrang, redV0ID',
 
     packages = ['python3_anticaptcha'],
     install_requires = [
                         'requests==2.21.0',
-                        'aiohttp==3.5.1',
+                        'aiohttp==3.5.3',
                         'pika==0.12.0'
     ],
     description = 'Python 3 AntiCaptcha library with AIO module.',
