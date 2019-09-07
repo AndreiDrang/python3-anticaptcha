b8007da62ef01b5f001c4b4389eb29ec4be766b0
Andrei <drang.andray@gmail.com>
2019-09-07 04:16:03 +0300

    Add recaptcha v3 tests in separate file

--- test.py
+++ test.py
@@ -154,34 +154,6 @@ class TestAntiCaptcha(object):
         assert default_init_params == init_params[0]
         assert default_handler_params == handler_params[0]
 
-    def test_fail_recaptcha_v3_proxyless(self):
-        recaptcha = ReCaptchaV3TaskProxyless.ReCaptchaV3TaskProxyless(
-            anticaptcha_key=self.anticaptcha_key_fail
-        )
-        # check response type
-        assert type(recaptcha) is ReCaptchaV3TaskProxyless.ReCaptchaV3TaskProxyless
-
-        response = recaptcha.captcha_handler(
-            websiteURL="https://www.google.com/recaptcha/api2/demo",
-            websiteKey="6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-",
-            minScore=0.3,
-            pageAction="login_test",
-        )
-        # check response type
-        assert type(response) is dict
-        # check all dict keys
-        assert ["errorId", "errorCode", "errorDescription"] == list(response.keys())
-        # check error code
-        assert response["errorId"] == 1
-
-        with pytest.raises(ValueError):
-            assert recaptcha.captcha_handler(
-                websiteURL="https://www.google.com/recaptcha/api2/demo",
-                websiteKey="6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-",
-                minScore=0.1,
-                pageAction="login_test",
-            )
-
     def test_true_recaptcha_v3_proxyless(self):
         recaptcha = ReCaptchaV3TaskProxyless.ReCaptchaV3TaskProxyless(
             anticaptcha_key=self.anticaptcha_key_true
