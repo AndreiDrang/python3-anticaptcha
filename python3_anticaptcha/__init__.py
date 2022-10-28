7a7c741198f03209d05a07c5785a2f13ac9f126e
Andrei <drang.andray@gmail.com>
2022-10-28 04:38:11 +0300

    Update __init__.py

--- python3_anticaptcha/__init__.py
+++ python3_anticaptcha/__init__.py
@@ -1,7 +1,3 @@
-from .config import (
-    app_key,
-    get_result_url,
-    create_task_url,
-)
+from .config import app_key, get_result_url, create_task_url
 from .errors import ReadError, IdGetError, ParamError
 from .get_answer import get_sync_result, get_async_result
