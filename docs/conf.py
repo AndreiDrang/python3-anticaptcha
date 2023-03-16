5171a5abf041ad52dfcf7550f9c668060bb8cf4a
Andrei <drang.andray@gmail.com>
2023-03-17 01:18:28 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ autodoc_typehints_format = "short"
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
+napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = False
 napoleon_use_admonition_for_examples = True
