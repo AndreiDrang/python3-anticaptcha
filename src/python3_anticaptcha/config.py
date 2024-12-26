69c81a093f9d1f4781ba61d9ef0c47bdc93c5440
Andrei <drop.all.requests@gmail.com>
2024-12-27 02:53:17 +0300

    Update config.py

--- src/python3_anticaptcha/config.py
+++ src/python3_anticaptcha/config.py
@@ -4,12 +4,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Адрес для создания задачи
-create_task_url = "https://api.anti-captcha.com/createTask"
-# Адрес для получения ответа
-get_result_url = "https://api.anti-captcha.com/getTaskResult"
-# ключ приложения
-app_key = "867"
+callbackUrl: str = ""
 
 
 # Connection retry generator
